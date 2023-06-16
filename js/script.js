5e939dc3fd8130643096b770ded259ad4057c17b
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-06-16 15:27:58 +0200

    [Bonus] Added Bonus

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
 // Controllo JS
 console.log("JSOK");
+
+// Controllo Vue
+
+console.log("Vue OK", Vue);
+
+// Estarpolo il metodo createApp
+
+const { createApp } = Vue;
+
+// Inizializzo l'app Vue
+
+const app = createApp({
+  data() {
+    return {
+      title: "Hello Word",
+      image: {
+        src: "https://picsum.photos/400/300",
+        alt: "random image",
+      },
+    };
+  },
+});
+
+// La monto nell'elemento HTML
+
+app.mount("#root");
